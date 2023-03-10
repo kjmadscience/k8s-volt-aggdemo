9e64dcceb85eb281bcb9e34cd881584906699fc8
kjmadscience <kjadeja@voltdb.com>
2023-03-10 10:54:39 +0000

    SQL path correction

--- reset.sh
+++ reset.sh
@@ -14,16 +14,16 @@ helm install mydb voltdb/voltdb --wait --values myproperties.yaml --version=1.8.
  --set-file cluster.config.licenseXMLFile="/Users/thanos/Documents/license.xml" --set metrics.enabled=true --set metrics.delta=true --namespace  volt
 
 sleep 90
-kubectl cp voltdb-aggdemo-createDB.sql mydb-voltdb-cluster-0:/tmp/ -n volt
+kubectl cp schema/voltdb-aggdemo-createDB.sql mydb-voltdb-cluster-0:/tmp/ -n volt
 
-kubectl cp procs.jar mydb-voltdb-cluster-0:/tmp/ -n volt
+kubectl cp schema/procs.jar mydb-voltdb-cluster-0:/tmp/ -n volt
 
-kubectl exec -it mydb-voltdb-cluster-0 -n volt -- sqlcmd < voltdb-aggdemo-createDB.sql
+kubectl exec -it mydb-voltdb-cluster-0 -n volt -- sqlcmd < schema/voltdb-aggdemo-createDB.sql
 
-kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create incoming_cdrs --partitions 24 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
+kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create incoming_cdrs --partitions 12 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
     
-kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create unaggregated_cdrs --partitions 24 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
+kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create unaggregated_cdrs --partitions 12 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
 
-kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create aggregated_cdrs --partitions 24 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
+kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create aggregated_cdrs --partitions 12 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
 
-kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create bad_cdrs --partitions 24 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
+kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create bad_cdrs --partitions 12 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
