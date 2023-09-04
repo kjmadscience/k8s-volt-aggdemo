8759314b68bc50597848f04e74c96a3e2392beed
jrmadscience <jadejakajal13@gmail.com>
2023-09-04 14:57:12 +0100

    new version changes

--- reset.sh
+++ reset.sh
@@ -10,11 +10,10 @@ sleep 30
 
 helm install redpanda redpanda/redpanda -n redpanda --values red.yaml
 
-helm install mydb voltdb/voltdb --wait --values myproperties.yaml --version=1.8.3 \
+helm install mydb voltdb/voltdb --wait --values myproperties.yaml --version=2.0.2 \
  --set-file cluster.config.licenseXMLFile="/Users/thanos/Documents/license.xml" --set metrics.enabled=true --set metrics.delta=true --namespace  volt
 
 sleep 90
-kubectl cp schema/voltdb-aggdemo-createDB.sql mydb-voltdb-cluster-0:/tmp/ -n volt
 
 kubectl cp schema/procs.jar mydb-voltdb-cluster-0:/tmp/ -n volt
 
@@ -22,8 +21,6 @@ kubectl exec -it mydb-voltdb-cluster-0 -n volt -- sqlcmd < schema/voltdb-aggdemo
 
 kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create incoming_cdrs --partitions 12 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
     
-kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create unaggregated_cdrs --partitions 12 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
-
 kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create aggregated_cdrs --partitions 12 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
 
 kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- rpk topic create bad_cdrs --partitions 12 --replicas 1 --brokers redpanda-0.redpanda.redpanda.svc.cluster.local.:9093,redpanda-1.redpanda.redpanda.svc.cluster.local.:9093,redpanda-2.redpanda.redpanda.svc.cluster.local.:9093
